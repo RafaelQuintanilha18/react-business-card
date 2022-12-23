3f27aa0dd1bce39b1859c5f767651a3d6801b0bc
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-12-23 18:07:14 +0100

    business card dark and light version

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Profile() {
     return (
         <div width="317px" className="profile">
-            <img src="./src/assets/profile.jpg" id="picture" alt="Profile Picture" width="317px" height="317px" />
+            <img src="/src/assets/profile.jpg" id="picture" alt="Profile Picture" width="317px" height="317px" />
             <h2>Rafael Quintanilha</h2>
             <h4>Frontend Developer</h4>
             <a href="http://troopl.com/rafaelquintanilha18" target="_blank"><p>rafael.website</p></a>
