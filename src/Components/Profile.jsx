b286b8d9ad37d8379bc812f97b12a2e6128f4a5d
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-12-23 18:26:57 +0100

    img fix

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import profile from "../assets/profile.jpg"
 
 export default function Profile() {
     return (
         <div width="317px" className="profile">
-            <img src="/src/assets/profile.jpg" id="picture" alt="Profile Picture" width="317px" height="317px" />
+            <img src={profile} id="picture" alt="Profile Picture" width="317px" height="317px" />
             <h2>Rafael Quintanilha</h2>
             <h4>Frontend Developer</h4>
             <a href="http://troopl.com/rafaelquintanilha18" target="_blank"><p>rafael.website</p></a>
